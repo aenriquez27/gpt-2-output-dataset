bef8ad434cf4daa7d787e96bf738d416a1f34036
aenriquez <alejandra.enriquez27@hotmail.com>
2024-03-03 11:02:30 -0800

    update tokenizer max len

--- detector/dataset.py
+++ detector/dataset.py
@@ -60,7 +60,7 @@ class EncodedDataset(Dataset):
         tokens = self.tokenizer.encode(text)
 
         if self.max_sequence_length is None:
-            tokens = tokens[:self.tokenizer.max_len - 2]
+            tokens = tokens[:self.tokenizer.model_max_length - 2]
         else:
             output_length = min(len(tokens), self.max_sequence_length)
             if self.min_sequence_length:
